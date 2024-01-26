606d7a9995ffadf159fafb19074b0fb5c924602f
22P31A0509 <119730222+22P31A0509@users.noreply.github.com>
2024-01-26 17:26:49 +0530

    import java.util.*; public class MyClass {     public static void main(String[] args)     {         Scanner read=new Scanner(System.in);         int x=read.nextInt();         int y=read.nextInt();         int z=read.nextInt();         int a=((x*5)+(y*10))/z;         System.out.println(a);     } }

--- Romeo_and_Juliet.java
+++ Romeo_and_Juliet.java
@@ -1,24 +1,13 @@
 import java.util.*;
-public class Num
+public class MyClass
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
-        int i;
-        Scanner m=new Scanner(System.in);
-        int a=m.nextInt();
-        int b=m.nextInt();
-        int c=m.nextInt();
-        int y=(a*5)+(b*10);
-        if(y<c)
-        {
-            System.out.println(0);
-        }
-        else
-        {
-            System.out.println(y/c);
-        }
-        
-       
-        
+        Scanner read=new Scanner(System.in);
+        int x=read.nextInt();
+        int y=read.nextInt();
+        int z=read.nextInt();
+        int a=((x*5)+(y*10))/z;
+        System.out.println(a);
     }
 }
